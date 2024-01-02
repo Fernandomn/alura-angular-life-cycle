70ad605f326e154998d90bb4134d3df610f5fb9f
Fernando Medeiros do Nascimento <fernando_m_n@hotmail.com>
2024-01-02 17:53:58 -0300

    addinf GormsModule. Refactoring InputComponent

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -3,11 +3,15 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input',
   templateUrl: './input.component.html',
-  styleUrls: ['./input.component.css']
+  styleUrls: ['./input.component.css'],
 })
 export class InputComponent implements OnInit {
+  itemValue!: string;
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void { }
+  addItem(): void {
+    console.log('this.itemValue', this.itemValue);
+  }
 }
